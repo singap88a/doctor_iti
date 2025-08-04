8cadb199ae28dddbe872e6aed342d34ccb8cc7cc
Asmaa Elmekawy <asmaa1182004@gmail.com>
2025-08-04 14:49:24 +0300

    expert doctors , activities ,servies , choose us

--- src/components/About_com/Activities.jsx
+++ src/components/About_com/Activities.jsx
@@ -1,10 +1,76 @@
 import React from 'react'
+ import Activities_1 from "../../assets/img_about/Activities_1.png";
+import Activities_2 from "../../assets/img_about/Activities_2.png";
+import Activities_3 from "../../assets/img_about/Activities_3.png";
+import Activities_4 from "../../assets/img_about/Activities_4.png";
+import Activities_5 from "../../assets/img_about/Activities_5.png";
+import Activities_6 from "../../assets/img_about/Activities_6.jpeg";
 
 export default function Activities() {
   return (
-    <div>
-      Activities
+    <>
+     <div className="mb-[50px]">
+      <div className="container px-4 py-16 mx-auto">
+        {/* Images Section */}
+        <div className="flex flex-col w-full gap-5 lg:flex-row lg:gap-10">
+          <div className="w-full lg:w-3/5">
+            <div className="flex flex-col justify-between gap-5 lg:flex-row">
+              <div className="mb-5 lg:mb-16">
+                <p className="text-lg font-semibold text-secondary">
+                  HAVE A LOOK AT
+                </p>
+                <h2 className="text-3xl font-bold lg:text-4xl text-text_color">
+                  Our Facilities and <br />
+                  Latest Activities
+                </h2>
+              </div>
+              <div className="lg:w-[48%] w-full mb-5">
+                <img
+                  src={Activities_6}
+                  alt="Facility 6"
+                  className="object-cover w-full rounded-[20px]"
+                />
+              </div>
+            </div>
+
+            {/* الصور بجانب بعض */}
+            <div className="grid grid-cols-2 gap-5">
+              <img
+                src={Activities_1}
+                alt="Facility 1"
+                className="object-cover w-full rounded-lg"
+              />
+              <img
+                src={Activities_2}
+                alt="Facility 2"
+                className="object-cover w-full rounded-lg"
+              />
+            </div>
+
+            <div className="mt-5">
+              <img
+                src={Activities_4}
+                alt="Facility 4"
+                className="object-cover w-full rounded-lg"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col w-full gap-5 lg:w-2/5">
+            <img
+              src={Activities_3}
+              alt="Facility 3"
+              className="object-cover w-full rounded-lg"
+            />
+            <img
+              src={Activities_5}
+              alt="Facility 5"
+              className="object-cover w-full rounded-lg"
+            />
+          </div>
+        </div>
+      </div>
     </div>
+    </>
   )
 }
  
--- src/components/About_com/Choose_Us.jsx
+++ src/components/About_com/Choose_Us.jsx
@@ -1,17 +1,60 @@
  import React from 'react'
+import Lottie from "lottie-react";
+import Choose_Us_anma from "../../Animation/Choose_Us.json";
+
  
  export default function Choose_Us() {
+  const Chooses =[
+    {
+      title:'Experienced Medical Professionals',
+      icon: 'fa-solid fa-user-md',
+      descriprtion: 'Our team includes experienced doctors, nurses, and other healthcare professionals who are dedicated to providing the best possible care to our patients.',
+    },
+     {
+      title:'Comprehensive Services',
+      icon: 'fa-solid fa-notes-medical',
+      descriprtion: 'We offer a wide range of healthcare services, from preventive care to specialized treatment for complex conditions.',
+    },
+     {
+      title:'Patient-centered Approach',
+      icon: 'fa-solid fa-hand-holding-heart',
+      descriprtion: 'We believe in treating each patient as an individual, and we take the time to understand your unique health needs and concerns.',
+    },
+     {
+      title: "State-of-the-art Facilities",
+      icon: "fa-solid fa-hospital",
+      descriprtion: 'Our healthcare center is equipped with the latest technology and equipment to provide our patients with the most advanced care possible.',
 
-
+    },
+  ]
    return (
      <>
-     <div className="mb-[100px] px-4">
+     <div className="mb-[100px] p-4">
       <div className="container mx-auto">
         <div className="flex flex-col gap-8 md:flex-row">
-          <div className="w-full text-center md:w-2/3">
+          <div className="flex items-center justify-center w-full md:w-1/3">
+            <Lottie animationData={Choose_Us_anma} className="w-full max-w-md" />
+          </div>
+          <div className="w-full md:w-2/3">
             <h1 className="mb-6 text-4xl font-bold text-text_color">
               Why Choose Us
             </h1>
+            <div className="grid pt-5 gap-6 sm:grid-cols-1 md:grid-cols-2">
+              {Chooses.map((choose, index) => (
+                <div
+                  className="p-6 "
+                  key={index}
+                >
+                  <i
+                    className={`text-4xl mb-4 text-secondary ${choose.icon}`}
+                  ></i>
+                  <h2 className="mb-2 text-2xl font-bold text-text_color">
+                    {choose.title}
+                  </h2>
+                  <p className="text-Paragraph">{choose.descriprtion}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
