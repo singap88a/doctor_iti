0a55617bc3d4b142404fa27c6ec0b328b1dcc1ee
Asmaa Elmekawy <asmaa1182004@gmail.com>
2025-08-06 15:36:46 +0300

    translate about page

--- src/components/About_com/Hero_about.jsx
+++ src/components/About_com/Hero_about.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import bg_hero from '../../assets/img_home/bg_hero.png'
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import bg_hero from '../../assets/img_home/bg_hero.png';
 
-export default function Hero_about({title,description,reverseLayout,img}) {
+export default function Hero_about({ titleKey, descriptionKey, reverseLayout, img }) {
+  const { t } = useTranslation();
+
   return (
-    <>
     <div
       className="w-full bg-center bg-cover mb-[100px]"
       style={{ backgroundImage: `url(${bg_hero})` }}
     >
-      <div className={`container flex flex-col justify-between py-12 ${reverseLayout ? 'md:flex-row-reverse' : 'md:flex-row'}`}> {/* إضافة التعديل هنا */}
-        {/* Image Sectionnn */}
+      <div className={`container flex flex-col justify-between py-12 ${reverseLayout ? 'md:flex-row-reverse' : 'md:flex-row'}`}>
+        {/* Image Section */}
         <div className="flex justify-center w-full md:w-1/2">
           <img
             src={img}
@@ -17,18 +19,18 @@ export default function Hero_about({title,description,reverseLayout,img}) {
             className="w-[80%] md:w-[70%] max-w-[400px] md:max-w-none"
           />
         </div>
-        {/* Text Sectionnn */}
+        {/* Text Section */}
         <div className="w-full pt-8 text-center md:w-1/2 md:text-left md:pt-28">
           <h1 className="text-3xl font-bold leading-snug md:text-6xl text-text_color">
-            {title}
+            {t(titleKey)}
           </h1>
           <p className="py-4 md:py-8 w-full md:w-[80%] mx-auto md:mx-0 text-Paragraph">
-            {description}
+            {t(descriptionKey)}
           </p>
         </div>
       </div>
     </div>
-    </>
-  )
+  );
 }
+
  
