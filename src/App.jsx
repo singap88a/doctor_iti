ede98fc2d098082b0a85d08435bcd09efee250fc
Ahmed Yasser Singap <130315264+singap88a@users.noreply.github.com>
2025-08-06 14:05:42 +0300

    abdallah-essam

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,44 @@ import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Doctors from "./pages/Doctors/Doctors";
 import Contact from "./pages/Contact/Contact";
+import DoctorDetails from "./pages/Doctors/DoctorDetail";
 import Gallery from "./pages/Gallery/Gallery";
 import Departments from "./pages/Departments/Departments";
 import DepartmentDetails from "./pages/Departments/DepartmentDetails";
+import Appointments from "./pages/Appointments/Appointments";
+import Timetable from "./pages/Timetable/Timetable";
+import ScrollToTop from "./ScrollToTop"; // Import ScrollToTop
  
-const App = () => {
+
+// import DoctorDetail from "./pages/Doctors/DoctorDetail";
+
+function App() {
   return (
     <div className="App">
       <Router>
+      <ScrollToTop /> {/* Add ScrollToTop here */}
+
         <Navbar />
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/doctor" element={<Doctors />} />
+          <Route path="/doctor/:id" element={<DoctorDetails />} />{" "}
           {/* This route will handle doctor details */}
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/contact" element={<Contact />} />
-           <Route path="/appointments" element={<Appointments />} />
-           <Route path="/departments" element={<Departments />} />
-           <Route path="/doctor" element={<Doctors />} />
-           <Route path="/department/:id" element={<DepartmentDetails />} />
+          <Route path="/departments" element={<Departments />} />
+          <Route path="/department/:id" element={<DepartmentDetails />} />
+          {/* Appointments */}
           <Route path="/appointments" element={<Appointments />} />
- 
+          {/* Timetable */}
+          <Route path="/timetable" element={<Timetable />} />
+          {/* Auth */}
+            {/* Admin */}
          </Routes>
-      </div>
-      <Footer />
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/components/Home_com/Departments.jsx
+++ src/components/Home_com/Departments.jsx
@@ -49,27 +49,6 @@ import Department from "../../pages/Departments/departmentsData";
       </div>
     </div>
      </>
- import { Link } from "react-router-dom"
-import bg_banner from "../../assets/img_home/bg_hero.png"
-import Department from "../../pages/Departments/departmentsData"
- 
- export default function Departments() {
-   return (
-     <div className="relative mb-[150px]">
-       <div>
-         <div 
-           className="container mx-auto bg-center bg-cover rounded-lg h-[770px] md:h-80 sm:px-8 lg:px-16"
-           style={{ backgroundImage: `url(${ bg_banner })` }}
-         >
-           <div className="flex items-center justify-center h-20 md:h-full">
-             <h2 className="text-3xl font-bold sm:text-4xl text-text_color">
-               departments
-             </h2>
-           </div>
-       </div>
-       </div>
-     </div>
->>>>>>> b270e47e0ffcfadb30c63dea852d381970317d7d
-   )
- }
+  );
+  }
  
