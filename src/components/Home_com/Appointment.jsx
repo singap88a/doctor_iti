212ad975f8da1bcaed20ed0ab2c3cb0157293883
Ahmed Yasser Singap <130315264+singap88a@users.noreply.github.com>
2025-08-17 01:10:30 +0300

    createAppointment

--- src/components/Home_com/Appointment.jsx
+++ src/components/Home_com/Appointment.jsx
@@ -97,7 +97,7 @@ function Appointment({ reverseLayout }) {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/appointments', {
+      const response = await fetch('https://backend-itiddoctor-395g.vercel.app/api/appointments', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
